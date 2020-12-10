0e129ade8b2cf60577f710108f31bace042bf9f6
HashtagOrNah <drghostlly@gmail.com>
2020-12-09 22:29:34 -0800

    added UI for starting game

--- app/mainMenuScripts.js
+++ app/mainMenuScripts.js
@@ -1,8 +1,10 @@
-function playGameOptions() {
+function playGameOptions(singpleyaer) {
 
     let menu_div = document.getElementById("main_menu_div");
+    let game_option_div = document.getElementById("game_options_div");
 
-    
+    menu_div.style.display = "none";
+    game_option_div.style.display = "block";
 }
 
 function validName() {
@@ -14,7 +16,20 @@ function validName() {
 
     if (nameTxt.value !== ""){
         name_div.style.display = "none";
-        menu_div.style.visibility = "visible";
+        menu_div.style.display = "block";
         welcomeTxt.innerHTML += `Welcome, ${nameTxt.value}!`
     }
+}
+
+function returnToMenu() {
+
+    let menu_div = document.getElementById("main_menu_div");
+    let game_option_div = document.getElementById("game_options_div");
+
+    menu_div.style.display = "block";
+    game_option_div.style.display = "none";
+}
+
+function startSingleplayer() {
+    
 }
